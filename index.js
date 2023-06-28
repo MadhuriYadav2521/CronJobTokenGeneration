a00f40a9cc39ea909ceba517a33097d5b49aa851
MadhuriYadav2521 <madhuri.y.0932@gmail.com>
2023-06-28 11:20:55 +0530

    updated

--- index.js
+++ index.js
@@ -3,19 +3,62 @@ import express from "express";
 import morgan from "morgan";
 import router from "./routes/apiRoutes.js";
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
+import path from 'path';
 
 const app = express();
+dotenv.config();
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }))
+
+const __dirname = path.resolve();
+
+// const app = express();
 
 app.use(morgan('dev'));
 app.use(express.json());
 app.use('/api/v1',router);
 
 
-mongoose.connect('mongodb+srv://madhuri13:diebold123@cluster0.ay3ihrp.mongodb.net/cronjobDB')
-.then( () => console.log("db connected"))
-.catch( (err) => console.log("error",err));
+// mongoose.connect('mongodb+srv://madhuri13:diebold123@cluster0.ay3ihrp.mongodb.net/cronjobDB')
+// .then( () => console.log("db connected"))
+// .catch( (err) => console.log("error",err));
+
+
+// app.listen(8000, () => console.log("working on port 8000"));
+
+
+
+app.get("/ping", (req, res) => {
+    return res.sendFile(__dirname + '/public/index.html');
+})
+
+
+app.get("/urlencoded", (req, res) => {
+    res.send(
+        `<form method='post' action='/login'>
+            <input name="email" placeholder="text" />
+            <input name="password"  placeholder="password"/>
+            <input type='submit' value="LOgin"/>
+        </form>`
+    )
+})
+
+app.post('/login', (req, res) => {
+    console.log(req.body.email)
+    console.log(req.body.password)
+    // db store 
+    res.send(`Your email ${req.body.email} and passsword is ${req.body.password}`)
+})
 
 
 
 
-app.listen(8000, () => console.log("working on port 8000"));
+
+mongoose.connect(process.env.MONGODB_URL)
+    .then(() => console.log("DB connected"))
+    .catch((err) => console.log("DB Error => ", err));
+
+
+app.listen(process.env.PORT, () => console.log("Working on port 8000")); // port
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,27 +1,22 @@
 import express from "express";
-import { getCurrencies, getLocals, getMarkets } from "../controllers/cultureAPIController.js";
-import { getCarriers } from "../controllers/carriersAPIController.js";
 import { getLocations } from "../controllers/geoAPIController.js";
 import { createSearch, getPollSearch, pollSearch } from "../controllers/Flight'sLivePrice-asyncContoller.js";
 import { createAndPollSearch } from "../controllers/Flight'sLivePrice-syncController.js";
-import { livePriceSearch, pollPageFromSearch } from "../controllers/hotelsLivePriceController.js";
-import { autosuggestFlights, autosuggestHotels } from "../controllers/autosuggestController.js";
 
 var router = express.Router();
 
-router.get('/getLocals',getLocals);
-router.get('/getCurrencies',getCurrencies);
-router.get('/getMarkets',getMarkets);
-router.get('/getCarriers',getCarriers);
+router.get('/ping', (req, res) => {
+    return res.send("Pong")
+})
+
+
 router.get('/getLocations',getLocations);
 router.post('/createSearch',createSearch);
 router.post('/pollSearch',pollSearch);
 router.get('/getPollSearch',getPollSearch);
 router.post('/createAndPollSearch',createAndPollSearch);
-router.post('/livePriceSearch',livePriceSearch);
-router.get('/pollPageFromSearch',pollPageFromSearch);
-router.post('/autosuggestFlights',autosuggestFlights);
-router.post('/autosuggestHotels',autosuggestHotels);
+
+
 
 
 
